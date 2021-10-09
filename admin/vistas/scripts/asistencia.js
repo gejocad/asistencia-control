b2b0d9f002441d51d3d72f766560683be35caa12
Gerald <gejocadev@gmail.com>
2021-10-09 16:15:01 -0500

    base control de asistencia

---
+++ admin/vistas/scripts/asistencia.js
@@ -0,0 +1,151 @@
+var tabla;
+
+//funcion que se ejecuta al inicio
+function init(){
+   listar();
+      listaru();
+$("#formulario").on("submit",function(e){
+   	guardaryeditar(e);
+   })
+
+    //cargamos los items al select cliente
+   $.post("../ajax/asistencia.php?op=selectPersona", function(r){
+   	$("#idcliente").html(r);
+   	$('#idcliente').selectpicker('refresh');
+   });
+
+}
+
+
+
+
+//funcion listar
+function listar(){
+	tabla=$('#tbllistado').DataTable({
+		"aProcessing": true,//activamos el procedimiento del datatable
+		"aServerSide": true,//paginacion y filrado realizados por el server
+		dom: 'Bfrtip',//definimos los elementos del control de la tabla
+		buttons: [
+                  'copyHtml5',
+                  'excelHtml5',
+                  'csvHtml5',
+                  'pdf'
+		],
+		"ajax":
+		{
+			url:'../ajax/asistencia.php?op=listar',
+			type: "get",
+			dataType : "json",
+			error:function(e){
+				console.log(e.responseText);
+			}
+		},
+		"bDestroy":true,
+		"iDisplayLength":10,//paginacion
+		"order":[[0,"desc"]]//ordenar (columna, orden)
+	});
+}
+function listaru(){
+	tabla=$('#tbllistadou').DataTable({
+		"aProcessing": true,//activamos el procedimiento del datatable
+		"aServerSide": true,//paginacion y filrado realizados por el server
+		dom: 'Bfrtip',//definimos los elementos del control de la tabla
+		buttons: [
+                  'copyHtml5',
+                  'excelHtml5',
+                  'csvHtml5',
+                  'pdf'
+		],
+		"ajax":
+		{
+			url:'../ajax/asistencia.php?op=listaru',
+			type: "get",
+			dataType : "json",
+			error:function(e){
+				console.log(e.responseText);
+			}
+		},
+		"bDestroy":true,
+		"iDisplayLength":10,//paginacion
+		"order":[[0,"desc"]],//ordenar (columna, orden)
+		"footerCallback": function () {
+        
+            total = this.api()
+                //.column(2)numero de columna a sumar
+                .column(3, {page: 'current'})//para sumar solo la pagina actual
+                .data()
+                .reduce(function (a, b) {
+                    return parseInt(a) + parseInt(b);
+                }, 0 );
+
+            $(this.api().column(3).footer()).html(total);
+            
+        }
+	});
+}
+
+
+
+function listar_asistencia(){
+var  fecha_inicio = $("#fecha_inicio").val();
+ var fecha_fin = $("#fecha_fin").val();
+ var idcliente = $("#idcliente").val();
+
+	tabla=$('#tbllistado_asistencia').dataTable({
+		"aProcessing": true,//activamos el procedimiento del datatable
+		"aServerSide": true,//paginacion y filrado realizados por el server
+		dom: 'Bfrtip',//definimos los elementos del control de la tabla
+		buttons: [
+                  'copyHtml5',
+                  'excelHtml5',
+                  'csvHtml5',
+                  'pdf'
+		],
+		"ajax":
+		{
+			url:'../ajax/asistencia.php?op=listar_asistencia',
+			data:{fecha_inicio:fecha_inicio, fecha_fin:fecha_fin, idcliente: idcliente},
+			type: "get",
+			dataType : "json",
+			error:function(e){
+				console.log(e.responseText);
+			}
+		},
+		"bDestroy":true,
+		"iDisplayLength":10,//paginacion
+		"order":[[0,"desc"]]//ordenar (columna, orden)
+	}).DataTable();
+}
+function listar_asistenciau(){
+var  fecha_inicio = $("#fecha_inicio").val();
+ var fecha_fin = $("#fecha_fin").val();
+
+	tabla=$('#tbllistado_asistenciau').dataTable({
+		"aProcessing": true,//activamos el procedimiento del datatable
+		"aServerSide": true,//paginacion y filrado realizados por el server
+		dom: 'Bfrtip',//definimos los elementos del control de la tabla
+		buttons: [
+                  'copyHtml5',
+                  'excelHtml5',
+                  'csvHtml5',
+                  'pdf'
+		],
+		"ajax":
+		{
+			url:'../ajax/asistencia.php?op=listar_asistenciau',
+			data:{fecha_inicio:fecha_inicio, fecha_fin:fecha_fin},
+			type: "get",
+			dataType : "json",
+			error:function(e){
+				console.log(e.responseText);
+			}
+		},
+		"bDestroy":true,
+		"iDisplayLength":10,//paginacion
+		"order":[[0,"desc"]]//ordenar (columna, orden)
+	}).DataTable();
+}
+
+
+
+init();
